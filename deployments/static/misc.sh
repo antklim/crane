bf4ab6be2ecd1a18575c9e8080be35e05380955c
antklim <antklim@gmail.com>
2021-03-02 09:34:00 +1100

    Code version added to build

--- deployments/static/misc.sh
+++ deployments/static/misc.sh
@@ -1,6 +1,6 @@
-docker build -t go-crane -f ./deployments/Dockerfile .
+docker build -t go-crane-static -f ./deployments/static/Dockerfile . --build-arg version=$(git rev-parse --short HEAD)
 
-docker run -d -v ~/.aws-lambda-rie:/aws-lambda --entrypoint /aws-lambda/aws-lambda-rie -p 9000:8080 go-crane:latest /aws_lambda
+docker run -d -v ~/.aws-lambda-rie:/aws-lambda --entrypoint /aws-lambda/aws-lambda-rie -p 9000:8080 go-crane-static:latest /static
 
 curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"foo": "bar123"}'
 
