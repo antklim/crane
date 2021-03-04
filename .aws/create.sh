10008dc6474b45e362ca22fd34c447036265c262
antklim <antklim@gmail.com>
2021-03-04 11:37:21 +1100

    Added ECR permissions, push to ECR

--- .aws/create.sh
+++ .aws/create.sh
@@ -7,6 +7,7 @@ aws cloudformation create-stack --stack-name crane-gh-actions-user \
   --parameters ParameterKey=AssetsBucket,ParameterValue=$ASSETS_BUCKET \
   ParameterKey=ExternalId,ParameterValue=$EXTERNAL_ID \
   ParameterKey=ProjectName,ParameterValue=$PROJECT \
+  ParameterKey=Repository,ParameterValue=$REPOSITORY \
   --tags Key=project,Value=$PROJECT \
   --region ap-southeast-2 \
   --capabilities CAPABILITY_NAMED_IAM \
